9fee555dbcf4e3c792fe127396bf4b9159055aa8
shibinjohn88 <shibinjohnpdlm@gmail.com>
2022-12-17 13:25:27 -0500

    changed component sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,13 +5,12 @@ function Sidebar () {
     return (
         <div className="sidebar">
         <h5>Customize Search</h5>
-        <ul>
-            <li><label><input type='checkbox'/> Search title only</label></li>
-            <li><label><input type='checkbox'/>has image</label></li>
-            <li><label><input type='checkbox'/>posted today</label></li>
-            <li><label><input type='checkbox'/>bundle duplabelcates</label></li>
-            <li><label><input type='checkbox'/>include nearby areas</label></li>     
-        </ul>
+        <div className="customize">
+            <label><input type='checkbox'/> Search title only</label>
+            <label><input type='checkbox'/> has image</label>
+            <label><input type='checkbox'/> posted today</label>
+            <label><input type='checkbox'/> include nearby areas</label>
+        </div>
         <Help />
         </div>
         
